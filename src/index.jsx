4b222595401d29c16cfb81db1c70c3bb97d4e3f6
Nnéna <nnena.osarokio@hotmail.fr>
2023-03-06 13:55:58 +0100

    2 - mise en pages Home/Freelances/Error/Header

--- src/index.jsx
+++ src/index.jsx
@@ -29,11 +29,19 @@ import Header from './components/Header'
 import Error from './components/Error'
 import Results from './pages/Results'
 import Freelances from './pages/Freelances'
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+    div {
+        font-family: 'Trebuchet MS', Helvetica, sans-serif;
+    }
+`
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <Router>
+      <GlobalStyle />
       <Header />
       <Routes>
         <Route exact path="/" element={<Home />}></Route>
@@ -42,9 +50,9 @@ root.render(
           path="/survey/:questionNumber"
           element={<Survey />}
         ></Route>
-        <Route element={<Error />}></Route>
         <Route exact path="/results" element={<Results />}></Route>
         <Route exact path="/freelances" element={<Freelances />}></Route>
+        <Route path="*" element={<Error />}></Route>
       </Routes>
     </Router>
   </React.StrictMode>
